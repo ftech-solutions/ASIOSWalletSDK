d1b257f6e847f06c00d8f9f83cf7442188b41c80
Muhammad Arslan <arslansaeed2646@gmail.com>
2022-02-19 20:05:39 +0500

    Latest Code

--- ASIOSWalletSDK/Classes/ASIOSWalletSDK.swift
+++ ASIOSWalletSDK/Classes/ASIOSWalletSDK.swift
@@ -1,14 +1,33 @@
 import UIKit
+import PassKit
 
 public class ASIOSWalletSDK: NSObject {
     
+    static var isTesting: Bool = false
+    static var sdkEnvironment: ASWalletEnviornment = .beta
+    
     public static func initSDK(environment: ASWalletEnviornment = .prod, debug: Bool = false) {
-        ASWallet.shared.isTesting = debug
-        ASWallet.shared.sdkEnvironment = environment
+        self.isTesting = debug
+        self.sdkEnvironment = environment
     }
     
     public static func isWalletInstalled() -> Bool {
-        return ASWallet.shared.getWalletStatus()
+        return deviceHasAppleWallet()
     }
     
 }
+
+func deviceHasAppleWallet() -> Bool {
+    if #available(iOS 10.0, *) {
+        return PKPaymentAuthorizationController.canMakePayments()
+    } else {
+        return false
+    }
+}
+
+public enum ASWalletEnviornment {
+    case beta
+    case preProd
+    case prod
+}
+
--- Example/ASIOSWalletSDK/ViewController.swift
+++ Example/ASIOSWalletSDK/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ASIOSWalletSDK.shared.initSDK(debug: true)
+        ASIOSWalletSDK.initSDK(environment: .beta, debug: true)
     }
 
     override func didReceiveMemoryWarning() {
